4aa48d86f1c347c874e3cdc88c985202f82b3701
Kiva Vlad <vladkiva9@gmail.com>
2020-09-29 02:44:17 +0300

    new update

--- authorization.php
+++ authorization.php
@@ -1,24 +1,9 @@
-<!DOCTYPE html>
-<html lang="ru">
+<?php
+$title = "Авторизация";
 
-<head>
-    <meta charset="utf-8">
-    <title>Авторизация</title>
-
-    <meta name="description" content="Fashion - интернет-магазин">
-    <meta name="keywords" content="Fashion, интернет-магазин, одежда, аксессуары">
-
-    <meta name="theme-color" content="#393939">
-
-    <link rel="preload" href="fonts/opensans-400-normal.woff2" as="font">
-    <link rel="preload" href="fonts/roboto-400-normal.woff2" as="font">
-    <link rel="preload" href="fonts/roboto-700-normal.woff2" as="font">
-
-    <link rel="icon" href="img/favicon.png">
-    <link rel="stylesheet" href="css/style.min.css">
-
-    <script src="js/scripts.js"></script>
-</head>
+//head
+require_once(__DIR__ . "/blocks/head.php");
+?>
 
 <body>
     <header class="page-header">
@@ -50,32 +35,10 @@
             <button class="button" type="submit">Войти в личный кабинет</button>
         </form>
     </main>
-    <footer class="page-footer">
-        <div class="container">
-            <a class="page-footer__logo" href="#">
-                <img src="img/logo--footer.svg" alt="Fashion">
-            </a>
-            <nav class="page-footer__menu">
-                <ul class="main-menu main-menu--footer">
-                    <li>
-                        <a class="main-menu__item" href="#">Главная</a>
-                    </li>
-                    <li>
-                        <a class="main-menu__item" href="#">Новинки</a>
-                    </li>
-                    <li>
-                        <a class="main-menu__item" href="index.html">Sale</a>
-                    </li>
-                    <li>
-                        <a class="main-menu__item" href="delivery.html">Доставка</a>
-                    </li>
-                </ul>
-            </nav>
-            <address class="page-footer__copyright">
-                © Все права защищены
-            </address>
-        </div>
-    </footer>
+    <?php 
+    //footer
+    require_once(__DIR__ . "/blocks/footer.php"); 
+    ?>
 </body>
 
 </html>
