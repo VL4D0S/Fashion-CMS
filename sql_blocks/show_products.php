34608f3c96f626aea74d059523f4db1e131f60f4
Kiva Vlad <vladkiva9@gmail.com>
2020-10-02 02:01:32 +0300

    new function description

--- sql_blocks/show_products.php
+++ sql_blocks/show_products.php
@@ -6,4 +6,3 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/mysql_connect.php");
 $sql = 'SELECT * FROM `products` ORDER BY `id` LIMIT 9';
 $query = mysqlConnect()->prepare($sql);
 $query->execute();
-
