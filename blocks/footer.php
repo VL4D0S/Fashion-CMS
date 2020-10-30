376ec5db4f5c0df51b67eddb4ef70ac55cd849bc
Kiva Vlad <vladkiva9@gmail.com>
2020-10-31 02:45:15 +0300

    new update

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,21 +1,21 @@
 <footer class="page-footer">
     <div class="container">
-        <a class="page-footer__logo" href="#">
+        <a class="page-footer__logo" href="/">
             <img src="../img/logo--footer.svg" alt="Fashion">
         </a>
         <nav class="page-footer__menu">
             <ul class="main-menu main-menu--footer">
                 <li>
-                    <a class="main-menu__item <?php if (($title === 'Fashion') && (!$_GET['cat1'])) echo 'active'?>" href="/">Главная</a>
+                    <a class="main-menu__item <?=(($title === 'Fashion') && (!isset($_GET['cat1']))) ? 'active' : ''?>" href="/">Главная</a>
                 </li>
                 <li>
-                    <a class="main-menu__item <?php if ($_GET['cat1'] == 'new') echo 'active'?>" href="/?cat1=new">Новинки</a>
+                    <a class="main-menu__item <?=($_GET['cat1'] === 'new') ? 'active' : ''?>" href="/?cat1=new">Новинки</a>
                 </li>
                 <li>
-                    <a class="main-menu__item <?php if ($_GET['cat1'] == 'sale') echo 'active'?>" href="/?cat1=sale">Sale</a>
+                    <a class="main-menu__item <?=($_GET['cat1'] === 'sale') ? 'active' : ''?>" href="/?cat1=sale">Sale</a>
                 </li>
                 <li>
-                    <a class="main-menu__item <?php if (($title === 'Доставка') && (!$_GET['cat1'])) echo 'active'?>" href="/delivery.php">Доставка</a>
+                    <a class="main-menu__item <?=(($title === 'Доставка') && (!isset($_GET['cat1']))) ? 'active' : ''?>" href="/delivery.php">Доставка</a>
                 </li>
             </ul>
         </nav>
--- blocks/header-menu.php
+++ blocks/header-menu.php
@@ -5,16 +5,16 @@
     <nav class="page-header__menu">
         <ul class="main-menu main-menu--header">
             <li>
-                <a class="main-menu__item <?php if (($title === 'Fashion') && (!isset($_GET['cat1']))) echo 'active'?>" href="/">Главная</a>
+                <a class="main-menu__item <?=(($title === 'Fashion') && (!isset($_GET['cat1']))) ? 'active' : ''?>" href="/">Главная</a>
             </li>
             <li>
-                <a class="main-menu__item <?php if ($_GET['cat1'] == 'new') echo 'active'?>" href="/?cat1=new">Новинки</a>
+                <a class="main-menu__item <?=($_GET['cat1'] === 'new') ? 'active' : ''?>" href="/?cat1=new">Новинки</a>
             </li>
             <li>
-                <a class="main-menu__item <?php if ($_GET['cat1'] == 'sale') echo 'active'?>" href="/?cat1=sale">Sale</a>
+                <a class="main-menu__item <?=($_GET['cat1'] === 'sale') ? 'active' : ''?>" href="/?cat1=sale">Sale</a>
             </li>
             <li>
-                <a class="main-menu__item <?php if (($title === 'Доставка') && (!isset($_GET['cat1']))) echo 'active'?>" href="/delivery.php">Доставка</a>
+                <a class="main-menu__item <?=(($title === 'Доставка') && (!isset($_GET['cat1']))) ? 'active' : ''?>" href="/delivery.php">Доставка</a>
             </li>
         </ul>
     </nav>
